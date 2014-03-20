8fc1c6de6dc60cdad1e3142e3c16a45286ea32a3
Omer Wazir <omer.wazir@gmail.com>
2014-03-20 09:22:02 -0700

    Added Jasmine to test exercise requirements

---
+++ tests/spec/ChapterTwoSpec.js
@@ -0,0 +1,21 @@
+describe("Exercise 2.1", function() {
+  
+  // beforeEach(function() {
+  //  //create things here?
+  // });
+	
+  describe("The grade object", function () {
+
+	  it("should  store a set of student grades", function() {
+	    expect(true).toBe(false);
+	  });
+
+	  it("should add the grades for each student", function() {
+	  	expect(true).toBe(false);
+	  });
+
+	  it("should provide a function to display the grade average for each student", function() {
+	  	expect(true).toBe(false);
+	  });
+  })  
+});
