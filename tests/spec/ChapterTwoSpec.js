cb076d735ab0ea5feb4aed501b05646c98cb2b5f
omer.wazir <omer.wazir@gmail.com>
2014-03-22 18:57:14 -0700

    Finished exercise 2.2
    Had to change the requirements since I don't know how to unit test
    output.  So I just created functions that construct a sentence from an
    array of words in forwards and backwards directions.

--- tests/spec/ChapterTwoSpec.js
+++ tests/spec/ChapterTwoSpec.js
@@ -24,4 +24,67 @@ describe("Exercise 2.1", function() {
 	  	expect(studentGrades.gradeAverage()).toBe(84);
 	  });
   })  
+
 });
+
+describe("Exercise 2.2", function(){
+
+	//requirement is not specific.
+	//so lets try to store a sentence into an array as words
+	//I don't know how to unit test what something displays
+	//so I will return it as a sentence forwards and backwards!
+	describe("The words array", function(){
+
+		beforeEach(function(){
+			var originalSentence = "the quick brown fox jumped over the lazy dog";
+			words = originalSentence.split(" ");
+			forwardSentence = function (array){
+				var i = 0;
+				var length = array.length;
+				var sentence = '';
+				array.forEach(function(element,index){
+					if(index === length - 1){
+						sentence += element;
+					} else {
+						sentence += element + ' ';
+					}
+				})
+				return sentence;
+			}
+
+			reverseSentence = function (array){
+				var clonedArray = array.slice(0);
+				clonedArray.reverse();
+				var i = 0;
+				var length = clonedArray.length;
+				var sentence = '';
+				clonedArray.forEach(function(element,index){
+					if(index === length - 1){
+						sentence += element;
+					} else {
+						sentence += element + ' ';
+					}
+				})
+				return sentence;
+			}
+		})
+
+		it("stores a set of words", function(){
+			//this could be a cheat since String.prototype.split()
+			//gives us an array of words
+			expect(words[0]).toBe('the');
+			expect(words[words.length -1]).toBe('dog');
+
+		});
+
+		it("can display the contents forward", function(){
+			var sentence = forwardSentence(words);
+			expect(sentence).toBe('the quick brown fox jumped over the lazy dog');
+		})
+
+		it("will also displays the contents backwards", function(){
+			var sentence = reverseSentence(words);
+			expect(sentence).toBe('dog lazy the over jumped fox brown quick the');
+		})
+	})
+})
